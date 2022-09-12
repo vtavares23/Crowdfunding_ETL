fd5f534f2e8b05f15e55637e9ff009ad13c42140
vtavares23 <victor.tavares@talladega.edu>
2022-09-12 00:30:38 -0500

    halfway

---
+++ Crowdfunding_analysis_starter.sql
@@ -0,0 +1,32 @@
+-- SQL Analysis
+-- 1.
+-- Retrieve number of projects for each outcome in descending order grouped by the outcome.
+
+
+
+-- 2. 
+-- Display the percentage of the goal amount in descending order, along with the campaign outcome, 
+-- the total goal and pledged amounts grouped by the campaign outcome.
+
+
+
+
+-- 3.
+-- Display the percentage of the goal amount in descending order, along with the category name, 
+-- the total goal and pledged amounts grouped by the category name.
+
+
+
+-- 4.
+-- Display the percentage of the goal amount in descending order, along with the subcategory name, 
+-- the total goal and pledged amounts grouped by the subcategory name.
+
+
+
+
+-- 5. 
+-- You need to send an email to the the contacts for all the "live" projects to let them know how much 
+-- is left of their goal amount. Display the first and last name, and email address of each contact,
+-- and the amount left to reach the goal for the "live" projects in descending order. 
+
+
